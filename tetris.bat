18b0f327413789c46cc822cda4af68fcfa1f8f65
Ivan Kosenko <foxartorg@gmail.com>
2021-11-19 14:38:53 +0200

    pause to install python

--- tetris.bat
+++ tetris.bat
@@ -1,7 +1,7 @@
 @echo off
-py --version 2>nul
+python --version 2>nul
 IF errorlevel 1 (
-   echo please install python https://www.python.org/ftp/python/3.9.9/python-3.9.9-amd64.exe
+   set /p DUMMY=please install python https://www.python.org/ftp/python/3.9.9/python-3.9.9-amd64.exe
 ) ELSE (
     pip install pygame
     python ./tetris.py
